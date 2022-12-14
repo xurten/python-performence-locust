162e6ea5f6e12b5f6994e659b468099397a52dd6
Andrzej <xurten@gmail.com>
2022-12-14 23:57:51 +0100

    Add more configuration

--- locustfiles/simple_example.py
+++ locustfiles/simple_example.py
@@ -1,10 +1,26 @@
-from locust import HttpUser, task
+import logging
+from http.client import HTTPConnection
+
+from locust import HttpUser, task, run_single_user
 
 
 class HelloWorldUser(HttpUser):
-    host = "http:/google.pl"
+    host = "http://google.pl"
 
     @task
     def hello_world(self):
         self.client.get("/")
-        self.client.get("/search?q=andrzej")
+        # self.client.get("/search?q=andrzej")
+
+
+def set_up_configuration():
+    HTTPConnection.debuglevel = 1
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+
+if __name__ == "__main__":
+    run_single_user(HelloWorldUser)
